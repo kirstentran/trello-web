0355e2e03ec71f4138833063ed7973fc3fd95fc4
Kirsten Tran <uyen.tran@usm.edu>
2024-09-19 00:53:24 -0500

    app_bar_p3

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,3 @@
-import { BorderColor } from '@mui/icons-material'
 import { deepOrange, teal, cyan, orange } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
@@ -24,6 +23,23 @@ const theme = extendTheme({
     }
   },
   components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#00b894'
+          }
+        }
+      }
+    },
     MuiButton: {
       styleOverrides: {
         root: {
