0204e60921f12abf81017fc40e0c3b142d86a9a2
Kirsten Tran <uyen.tran@usm.edu>
2024-09-24 11:00:54 -0500

    board_content_part_2

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,11 +1,11 @@
 import Container from '@mui/material/Container'
-import AppBar from '~/components/AppBar'
-import BoardBar from './BoardBar'
-import BoardContent from './BoardContent'
+import AppBar from '~/components/AppBar/AppBar'
+import BoardBar from './BoardBar/BoardBar'
+import BoardContent from './BoardContent/BoardContent'
 
 function Board() {
   return (
-    <Container disableGutters maxWidth ={false} sx={{ height: '100vh'}}>
+    <Container disableGutters maxWidth ={false} sx={{ height: '100vh' }}>
       <AppBar />
       <BoardBar/>
       <BoardContent/>
