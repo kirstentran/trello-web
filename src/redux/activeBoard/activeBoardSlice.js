736f7935469f42f4a2b61bd18efe051c4b03f559
Kirsten Tran <tn.phuonguyen1010@gmail.com>
2024-11-20 11:21:16 -0600

    lesson_5

--- src/redux/activeBoard/activeBoardSlice.js
+++ src/redux/activeBoard/activeBoardSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios'
+import authorizedAxiosInstance from '~/utils/authorizeAxios'
 import { API_ROOT } from '~/utils/constants'
 import { mapOrder } from '~/utils/sorts'
 import { generatePlaceHolderCard } from '~/utils/formatters'
@@ -13,7 +13,7 @@ const initialState = {
 export const fetchBoardDetailsAPI = createAsyncThunk(
   'activeBoard/fetchBoardDetailsAPI',
   async (boardId) => {
-    const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
+    const response = await authorizedAxiosInstance.get(`${API_ROOT}/v1/boards/${boardId}`)
     //Axios sẽ trả kết quả về thông qua property của nó là data
     return response.data
   }
